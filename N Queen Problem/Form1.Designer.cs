1935b1982dcfe02ffb195f3b7a02d080ffea5d80
safayatzisan <zisan_cse_mbstu@hotmail.com>
2016-12-12 12:58:36 +0600

    Add project files.

---
+++ N Queen Problem/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace N_Queen_Problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.Location = new System.Drawing.Point(242, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 36);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 37);
+            this.label1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(242, 147);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(461, 414);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(41, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter N Here :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(-5, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(242, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Queens\' Position :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Buxton Sketch", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(-3, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 29);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "N Queen Problem";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(764, 587);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "N Queen";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ N Queen Problem/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N_Queen_Problem
+{
+    public partial class Form1 : Form
+    {
+        static int N;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                N = int.Parse(textBox1.Text);
+            }
+            int[,] board = new int[N,N];
+		    if(!theBoardSolver(board, 0))
+		    {
+			    label1.Text="Solution not found\n";
+		    }
+		    printBoard(board);
+        }
+
+        private void printBoard(int[,] board)
+        {
+            StringBuilder strb = new StringBuilder();
+            int i;
+            strb.Clear();
+		    for(i=0;i<N;i++)
+		    {
+			    for(int j=0;j<N;j++)
+				    if(board[i,j]==1)
+				    {
+                        strb.Append("Q    ");
+				    }
+				    else
+				    {
+                        strb.Append("-    ");
+				    }
+                    strb.Append("\n");
+		    }
+            label1.Text = strb.ToString();
+        }
+
+        private bool theBoardSolver(int[,] board, int col)
+        {
+            if (col >= N)
+                return true;
+            for (int i = 0; i < N; i++)
+            {
+                if (toPlaceOrNotToPlace(board, i, col))
+                {
+                    board[i,col] = 1;
+                    if (theBoardSolver(board, col + 1))
+                    {
+                        return true;
+                    }
+                    board[i,col] = 0;
+                }
+            }
+            return false;
+        }
+
+        private bool toPlaceOrNotToPlace(int[,] board, int row, int col)
+        {
+            int i, j;
+            for (i = 0; i < col; i++)
+            {
+                if (board[row,i] == 1)
+                {
+                    return false;
+                }
+            }
+            for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            {
+                if (board[i,j] == 1)
+                {
+                    return false;
+                }
+            }
+            for (i = row, j = col; j >= 0 && i < N; i++, j--)
+            {
+                if (board[i,j] == 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label4.Location = new Point(label4.Location.X + 5, label4.Location.Y);
+
+            if (label4.Location.X > this.Width)
+            {
+                label4.Location = new Point(0 - label4.Width, label4.Location.Y);
+            }
+        }
+    }
+}
